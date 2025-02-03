1825021ad50e741b3a6cb8808b20afdfc90af196
Eric <ericlauchiho@gmail.com>
2025-02-03 23:06:05 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+    base: process.env.GITHUB_PAGES
+        ? "ms-teams-status-defender"
+        : "./",
+    plugins: [react()],
+    build: {
+      outDir: './docs',
+      emptyOutDir: true,
+    },
 });
